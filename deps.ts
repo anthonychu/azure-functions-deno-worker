c191da6038ccc816d6192bb3dbd953560be9d83b
Anthony Chu <anthony@anthonychu.ca>
2020-05-14 20:05:14 -0700

    Pin package versions (temp jszip version)

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { parse } from "https://deno.land/std/flags/mod.ts";
-export { readZip } from "https://deno.land/x/jszip/mod.ts";
-export { ensureDir } from "https://deno.land/std/fs/ensure_dir.ts";
-export { move } from "https://deno.land/std/fs/move.ts";
-export { walk } from "https://deno.land/std/fs/walk.ts";
-export { Application, Router, Context as OakContext, Body } from "https://deno.land/x/oak/mod.ts";
+export { parse } from "https://deno.land/std@0.51.0/flags/mod.ts";
+export { readZip } from "https://raw.githubusercontent.com/anthonychu/deno-zip/std-v0.51.0/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.51.0/fs/ensure_dir.ts";
+export { move } from "https://deno.land/std@0.51.0/fs/move.ts";
+export { walk } from "https://deno.land/std@0.51.0/fs/walk.ts";
+export { Application, Router, Context as OakContext, Body } from "https://deno.land/x/oak@v4.0.0/mod.ts";
