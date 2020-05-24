b09f8d3a9b3ea5b03a34ab2d850a31c6350a319a
Anthony Chu <antchu@microsoft.com>
2020-05-24 14:30:35 -0700

    Update to std@0.53.0

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { parse } from "https://deno.land/std@0.51.0/flags/mod.ts";
-export { readZip } from "https://raw.githubusercontent.com/anthonychu/deno-zip/std-v0.51.0/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.51.0/fs/ensure_dir.ts";
-export { move } from "https://deno.land/std@0.51.0/fs/move.ts";
-export { walk } from "https://deno.land/std@0.51.0/fs/walk.ts";
+export { parse } from "https://deno.land/std@0.53.0/flags/mod.ts";
+export { readZip } from "https://raw.githubusercontent.com/anthonychu/deno-zip/std-v0.53.0/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.53.0/fs/ensure_dir.ts";
+export { move } from "https://deno.land/std@0.53.0/fs/move.ts";
+export { walk } from "https://deno.land/std@0.53.0/fs/walk.ts";
 export { Application, Router, Context as OakContext, Body } from "https://deno.land/x/oak@v4.0.0/mod.ts";
