91c31318944db99d687d20b271f8571191dc0bfd
Anthony Chu <anthony@anthonychu.ca>
2020-12-05 23:27:36 -0800

    Update for Deno 1.5.4 (#28)
    * Update workflow
    
    * Update action
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Add matrix
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Add macos
    
    * Update workflow
    
    * Add Az login
    
    * Add publish
    
    * Run tests after deployment
    
    * Fix workflow
    
    * Update workflow
    
    * add `await` for oak@6.x
    
    * fix `parseBody` func
    
    * Add import/export type, --no-check
    
    * Add --no-check to workflow
    
    * Add debug statements to workflow
    
    * Update debug statements
    
    * Update workflow
    
    * Update for Deno 1.5.4
    
    * Update workflow
    
    Co-authored-by: horihiro <hori.kawa.shima.tool+github@gmail.com>

--- mod.ts
+++ mod.ts
@@ -1,10 +1,12 @@
 import { Application, Router, OakContext, Body } from "./worker_deps.ts";
-import {
+import type {
   AzureFunction,
+  HttpMethod,
+} from "./types.ts";
+import type {
   Context,
   Logger,
   HttpRequest,
-  HttpMethod,
 } from "./types.ts";
 
 export interface FunctionRegistration {
@@ -51,12 +53,12 @@ class FunctionHttpRequest implements HttpRequest {
   }
 }
 
-function parseBody(body: { type: string; value: any }) {
-  let value = body.value;
+async function parseBody(body: { type: string; value: any }) {
+  let value = await body.value;
   if (body.type === "text") {
     try {
-      value = JSON.parse(body.value);
-    } catch {}
+      value = JSON.parse(value);
+    } catch { }
   }
   return value;
 }
@@ -64,7 +66,7 @@ function parseBody(body: { type: string; value: any }) {
 function tryJsonParse(input: any) {
   try {
     input = JSON.parse(input);
-  } catch {}
+  } catch { }
   return input;
 }
 
@@ -111,78 +113,34 @@ export class AzureFunctionsWorker {
             };
           }
 
-          let parsedBody: any = parseBody(body);
+          let parsedBody: any = await parseBody(body);
 
           const isHttpPassthrough: boolean = parsedBody === undefined ||
             !(parsedBody.Data && parsedBody.Metadata);
           const context = new FunctionContext();
 
-          if (isHttpPassthrough) {
-            // http passthrough
-            const req = new FunctionHttpRequest(ctx.request.method);
-            req.body = parsedBody;
-            req.rawBody = typeof (req.body) === "string"
-              ? req.body
-              : JSON.stringify(req.body);
-            for (const h of ctx.request.headers) {
-              req.headers[h[0]] = h[1];
-            }
-            // TODO: load query and other fields
-            context.req = req;
-          } else {
-            // lots of stuff need camelcasing
-            // TODO: refactor
-            parsedBody.Metadata.sys = tryJsonParse(parsedBody.Metadata.sys);
-            toCamelCaseKeys(parsedBody.Metadata.sys);
-            toCamelCaseKeys(parsedBody.Data.req);
+          // lots of stuff need camelcasing
+          // TODO: refactor
+          parsedBody.Metadata.sys = tryJsonParse(parsedBody.Metadata.sys);
+          toCamelCaseKeys(parsedBody.Metadata.sys);
+          toCamelCaseKeys(parsedBody.Data.req);
 
-            context.req = parsedBody.Data.req;
-            for (const [key, value] of Object.entries(parsedBody.Data)) {
-              context.bindings[toCamelCase(key)] = tryJsonParse(value);
-            }
-            for (const [key, value] of Object.entries(parsedBody.Metadata)) {
-              context.bindingData[toCamelCase(key)] = tryJsonParse(value);
-            }
+          context.req = parsedBody.Data.req;
+          for (const [key, value] of Object.entries(parsedBody.Data)) {
+            context.bindings[toCamelCase(key)] = tryJsonParse(value);
+          }
+          for (const [key, value] of Object.entries(parsedBody.Metadata)) {
+            context.bindingData[toCamelCase(key)] = tryJsonParse(value);
           }
 
           const result = await Promise.resolve(registration.handler(context));
 
-          if (isHttpPassthrough) {
-            const httpOutputBinding = registration.metadata.bindings.find((
-              b: any,
-            ) => b.type === "http" && b.direction === "out");
-
-            let funcResponse: any;
-            if (httpOutputBinding.name === "$return") {
-              if (typeof (result) === "string") {
-                funcResponse = {
-                  status: !!result ? 200 : 204,
-                  body: result,
-                };
-              } else {
-                funcResponse = result;
-              }
-            } else {
-              funcResponse = context.res;
-            }
-
-            ctx.response.status = funcResponse.status;
-            ctx.response.body = funcResponse.body;
-            ctx.response.headers = new Headers();
-
-            if (funcResponse.headers) {
-              for (const [key, value] of Object.entries(funcResponse.headers)) {
-                ctx.response.headers.set(key as string, value as string);
-              }
-            }
-          } else {
-            ctx.response.body = {
-              Logs: context.log.logs,
-              Outputs: {},
-              ReturnValue: result,
-            };
-            ctx.response.headers.set("content-type", "application/json");
-          }
+          ctx.response.body = {
+            Logs: context.log.logs,
+            Outputs: context,
+            ReturnValue: result,
+          };
+          ctx.response.headers.set("content-type", "application/json");
         } catch (ex) {
           console.error(ex);
           ctx.response.status = 500;
@@ -236,7 +194,7 @@ export class AzureFunctionsWorker {
     for (const func of this.#functionRegistrations) {
       try {
         await Deno.mkdir(func.name);
-      } catch {}
+      } catch { }
       const encoder = new TextEncoder();
       const data = encoder.encode(JSON.stringify(func.metadata, null, 2));
       console.info(`Generating file ${func.name}/function.json.`);
--- worker_deps.ts
+++ worker_deps.ts
@@ -1,6 +1,8 @@
+export type {
+  Body,
+} from "https://deno.land/x/oak@v6.1.0/mod.ts";
 export {
   Application,
   Router,
   Context as OakContext,
-  Body,
-} from "https://deno.land/x/oak@v6.0.2/mod.ts";
+} from "https://deno.land/x/oak@v6.1.0/mod.ts";
